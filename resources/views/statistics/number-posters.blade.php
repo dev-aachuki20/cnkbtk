d5d4908d431a2322c0a6066c7d35f11983c25361
Aachukihipl <aachukiagarwal.hipl@gmail.com>
2024-04-22 14:49:14 +0530

    remove code

--- resources/views/statistics/number-posters.blade.php
+++ resources/views/statistics/number-posters.blade.php
@@ -58,19 +58,5 @@
     var myChart = new Chart(ctx, config);
 
    
-    $(document).ready(function() {
-        $('#filter').change(function() {
-            var filter = this.value;
-            $.ajax({
-                url: "{{ route('admin.statistics.number-posters') }}/" + filter,
-                type: 'GET',
-                success: function(response) {
-                    $(".profile-content").html(response.html);
-                },
-                error: function(xhr, status, error) {
-                    console.error(error);
-                }
-            });
-        });
-    });
+    
 </script>
