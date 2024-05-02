fed5843c8b3e2e7ec3c34e82b13215e2b429e2e8
“chetankumawatHIPL” <chetankumawat.his@gmail.com>
2024-05-02 11:05:04 +0530

    design changes

--- resources/views/blacklist-user/index.blade.php
+++ resources/views/blacklist-user/index.blade.php
@@ -78,7 +78,7 @@
                                     <div class="col-auto d-flex">
                                         <!-- Excel import Form -->
                                         <form class="m-0 px-3" id="excelForm" action="{{ route('blacklist.user.import') }}" method="POST" enctype="multipart/form-data">
-                                            <label for="excel_file" class="btn btn-primary p-0 text-white">{{__('pages.blacklist_user.button_two')}}</label>
+                                            <label for="excel_file" class="btn btn-primary px-3 d-flex align-items-center">{{__('pages.blacklist_user.button_two')}}</label>
                                             <input type="file" name="excel_file" id="excel_file" value="Import Excel" hidden>
                                         </form>
                                     </div>
