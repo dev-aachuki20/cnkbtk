1ce7ea634bf5c9c2be6bbaa793206335cd79757f
Aachukihipl <aachukiagarwal.hipl@gmail.com>
2024-07-04 16:43:06 +0530

    add production key for paypal

--- config/paypal.php
+++ config/paypal.php
@@ -7,9 +7,10 @@ return [
         'client_secret' => env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
     ],
     'live' => [
-        'client_id' => env('PAYPAL_LIVE_CLIENT_ID', ''),
-        'client_secret' => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
+        'client_id' => env('PAYPAL_CLIENT_ID', ''),
+        'client_secret' => env('PAYPAL_SECRET', ''),
     ],
+    'payment_action' => 'Pay',
     'currency' => env('PAYPAL_CURRENCY', 'USD'),
     'validate_ssl' => env('PAYPAL_VALIDATE_SSL', true),
 ];
