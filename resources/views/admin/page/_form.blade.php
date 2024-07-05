9fdf01f9cea94ec700e080ef959a62b47b53206d
Mayurhis <chetankumawat.his@gmail.com>
2024-07-05 16:53:17 +0530

    fixed scrollbar design in multiple browser

--- resources/views/admin/page/_form.blade.php
+++ resources/views/admin/page/_form.blade.php
@@ -1,34 +1,30 @@
 {{-- for english --}}
-<div class="row">
-<div class="col-md-6">
-    <div class="card-body">
-        <div class="form-group {{ $errors->has('content_en') ? 'has-error' : '' }}">
-            <label>{{ trans('cruds.pages.fields.content') }} ({{ trans('global.english') }}) <span class="required">*</span></label>
-            <textarea id="contentEn" name="content_en">
-                {{ isset($pageRecord) ? $pageRecord[0]['content_en'] : '' }}    
-        </textarea>
-            @if ($errors->has('content_en'))
-                <span class="text-danger">{{ $errors->first('content_en') }}</span>
-            @endif
+<div class="card-body">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group {{ $errors->has('content_en') ? 'has-error' : '' }}">
+                <label>{{ trans('cruds.pages.fields.content') }} ({{ trans('global.english') }}) <span class="required">*</span></label>
+                <textarea id="contentEn" name="content_en">
+                    {{ isset($pageRecord) ? $pageRecord[0]['content_en'] : '' }}    
+                </textarea>
+                @if ($errors->has('content_en'))
+                    <span class="text-danger">{{ $errors->first('content_en') }}</span>
+                @endif
+            </div>
+        </div>
+        {{-- for chinese --}}
+        <div class="col-md-6">
+            <div class="form-group {{ $errors->has('content_ch') ? 'has-error' : '' }}">
+                <label>{{ trans('cruds.pages.fields.content') }} ({{ trans('global.chinese') }}) <span class="required">*</span></label>
+                <textarea id="contentCh" name="content_ch">
+                    {{ isset($pageRecord) ? $pageRecord[0]['content_ch'] : '' }}    
+                </textarea>
+                @if ($errors->has('content_ch'))
+                    <span class="text-danger">{{ $errors->first('content_ch') }}</span>
+                @endif
+            </div>
         </div>
     </div>
-</div>
-
-
-{{-- for chinese --}}
-<div class="col-md-6">
-    <div class="card-body">
-        <div class="form-group {{ $errors->has('content_ch') ? 'has-error' : '' }}">
-            <label>{{ trans('cruds.pages.fields.content') }} ({{ trans('global.chinese') }}) <span class="required">*</span></label>
-            <textarea id="contentCh" name="content_ch">
-                {{ isset($pageRecord) ? $pageRecord[0]['content_ch'] : '' }}    
-        </textarea>
-            @if ($errors->has('content_ch'))
-                <span class="text-danger">{{ $errors->first('content_ch') }}</span>
-            @endif
-        </div>
-    </div>
-</div>
 </div>
 {{-- update button --}}
 <div class="card-footer">
